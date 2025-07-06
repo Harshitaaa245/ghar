bb08a23a4078ad7aeaf585c5ef2207fc647443cb
Harshitaaa245 <harshitamemory@gmail.com>
2025-07-06 13:39:16 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-5xl font-bold text-green-800">Ghar 🌿</h1>
+    </div>
+  );
+}
 
